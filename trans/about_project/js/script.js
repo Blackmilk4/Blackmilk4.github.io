8bbdb0008742769861143be87d32300e41da1370
Blackmilk4 <Hakudeito@mail.ru>
2019-06-21 19:47:36 +0400

    321312321
    31231313213

---
+++ trans/about_project/js/script.js
@@ -0,0 +1,36 @@
+$('a[href^="#"]').bind("click", function (e) {
+	var anchor = $(this);
+	$('html, body').stop().animate({
+		scrollTop: $(anchor.attr("href")).offset().top
+	}, 1000);
+	e.preventDefault();
+	return false
+});
+
+
+
+
+var swiper = new Swiper('.swiper-container', {
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'fraction',
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
+
+
+
+
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		openEffect: 'none',
+		closeEffect: 'none',
+		iframe: {
+			preload: false
+		}
+	});
+});
